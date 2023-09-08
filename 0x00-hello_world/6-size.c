481f076dd92a2fa972c9067985a2ac4575508748
OMAAHR <jeffersongrey82@gmail.com>
2023-09-07 19:02:31 -0700

    types of computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %lu byte(s)", sizeof(char));
-printf("size of an int: %lu byte(s)", sizeof(int));
-printf("size of a long int: %lu byte(s)", sizeof(long int));
-printf("size of a long long int: %lu byte(s)", sizeof(long long int));
-printf("size of a float: %lu byte(s)", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("size of an int: %lu byte(s)\n", sizeof(int));
+printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
