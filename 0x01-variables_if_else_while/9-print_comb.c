7ff97895d8bf1899bdf663c15e0a1b7548b31fd0
OMAAHR <jeffersongrey82@gmail.com>
2023-09-11 05:44:33 -0700

    comination of single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Description - showing exceptions
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	if (c <= '8')
+	{
+	putchar(',');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
