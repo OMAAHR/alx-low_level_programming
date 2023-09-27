8edec33edd3b57f57a548384bea0aa9495b49ad0
OMAAHR <jeffersongrey82@gmail.com>
2023-09-26 19:35:33 -0700

    search string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,14 +13,14 @@ char *_strpbrk(char *s, char *accept)
 	int i;
 
 	while (*s)
-	{
-
-	for (i = 0; accept[i];  i++)
 		{
-			if (*s == accept[i])
+
+		for (i = 0; accept[i];   i++)
+		{
+		if (*s == accept[i])
 			return (s);
 		}
 		s++;
-	}
+		}
 	return ('\0');
 }
